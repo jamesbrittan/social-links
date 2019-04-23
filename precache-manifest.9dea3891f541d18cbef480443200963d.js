f63b573c8729d3304660ee277a4597de5cfd6c2d
James Brittan <james_brittan@shelter.org.uk>
2019-04-23 16:22:08 +0100

    Updates

---
+++ precache-manifest.9dea3891f541d18cbef480443200963d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "098627abf7845f9d23bf",
+    "url": "/social-links/static/js/runtime~main.24651f84.js"
+  },
+  {
+    "revision": "2b309510210c5d204d5d",
+    "url": "/social-links/static/js/main.1be56c6d.chunk.js"
+  },
+  {
+    "revision": "b4ca7cd6881bd6d8548d",
+    "url": "/social-links/static/js/2.9ada3c83.chunk.js"
+  },
+  {
+    "revision": "2b309510210c5d204d5d",
+    "url": "/social-links/static/css/main.4e9bafb4.chunk.css"
+  },
+  {
+    "revision": "7182806e85f186b3a810d7267b981753",
+    "url": "/social-links/index.html"
+  }
+];
